eeee6a569f4ee319c5a72b44d190ad34b4d7cb16
Haron1248 <natn0367@gmail.com>
2024-05-02 15:21:28 +0900

    chore: db 연동 세팅 업데이트

--- app/database.py
+++ app/database.py
@@ -1,16 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from dotenv import load_dotenv
+import os
 
 # 데이터베이스 접속 주소
-SQLALCHEMY_DATABASE_URL = ""
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+DATABASE_URL = f'mysql+pymysql://{os.environ["sql_db_user"]}:{os.environ["sql_db_password"]}@{os.environ["sql_db_address"]}:{os.environ["sql_db_port"]}/{os.environ["sql_db_connect"]}'
 
 # 데이터베이스 사용 규칙
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_sam_thread": False}
+    DATABASE_URL
 )
+
 SessionLocal = sessionmaker(
     autocommit=False, 
     autoflush=False, 
