f1ffd423264d7cdf44040d08d4990daed79d0bf4
Haron1248 <natn0367@gmail.com>
2024-05-06 16:26:31 +0900

    feat: 버스 유형에 따른 색상 적용

--- app/api/bus.py
+++ app/api/bus.py
@@ -1,5 +1,44 @@
 import httpx
 
+busColor = {
+    # RED
+    "광역" : "#F72F08",
+    "직행" : "#F72F08",
+
+    # GREEN
+    "지선" : "#5BB025",
+    "맞춤" : "#5BB025",
+    "마을" : "#5BB025",
+
+    # BLUE
+    "간선" : "#3D5BAB",
+    "심야" : "#3D5BAB",
+    "좌석" : "#3D5BAB",
+    "일반" : "#3D5BAB",
+
+    # YELLOW
+    "순환" : "#F99D1C",
+    "투어" : "#F99D1C",
+
+    # BROWN
+    "공항" : "#8B4513",
+
+    # PURPLE
+    "급행" : "#6E2DB9",
+
+    # Another
+    "BRT" : "#CB2B2B",
+    "두루타" : "#00A0C6",
+    "누비다" : "#FF69B4",
+    "임시" : "#FFBC00",
+    "행복" : "#FA5882",
+    "읍면" : "#5FBF15",
+    "마실" : "#FFBC00",
+    "출근" : "#82D438",
+    "군위" : "#002187",
+    "폐선" : "#AAAAAA"
+}
+
 # GET Method
 async def get_busList(url: str):
     async with httpx.AsyncClient() as client:
@@ -13,7 +52,7 @@ async def get_busList(url: str):
                         "bus_id": temp["routeid"],
                         "name": temp["routeno"],
                         "type": temp["routetp"][0:-2],
-                        "color": "#aaffaa",
+                        "color": busColor.get(temp["routetp"][0:-2]),
                         "station_left": temp["arrprevstationcnt"],
                         "time_left": temp["arrtime"]
                     })
