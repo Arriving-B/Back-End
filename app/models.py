b03d46bdd39e19c1c95eeccf64e1c621ab274e68
Haron1248 <natn0367@gmail.com>
2024-05-08 13:46:54 +0900

    refactor: 불필요 라이브러리 삭제

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Time, ForeignKey, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func
 from sqlalchemy.orm import relationship
 
 from database import Base
